be834826b776cdd9e753a1fca429d199bc7daa01
Zach Manno <zdm5045@psu.edu>
2020-09-17 17:57:44 -0400

    add simple login for app, still needs infra

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
 from flask import Flask
+from flask_simplelogin import SimpleLogin
+import os
+
+
+def load_configs():
+    users_list = ['DANNY_LOGIN_USER_PWD', 'HEINS_LOGIN_USER_PWD', 'PAT_LOGIN_USER_PWD', 'MANNO_LOGIN_USER_PWD']
+    login_dict = dict()
+    for user in users_list:
+        userpwd_combo = os.environ.get(user).split('-')
+        username = userpwd_combo[0]
+        pwd = userpwd_combo[1]
+        login_dict[username] = pwd
+
+    secret_key = os.environ.get('LOGIN_SECRET_KEY')
+    return secret_key, login_dict
+
+
+secret_key, user_login_dict = load_configs()
+
+print('User login dict: ' + str(user_login_dict))
+
+
+def only_jimjams_users_login(user):
+    """:param user: dict {'username': 'foo', 'password': 'bar'}"""
+    username = user.get('username')
+    pwd_provided = user.get('password')
+    if username in user_login_dict:
+        if pwd_provided == user_login_dict[username]:
+            return True  # <--- Allowed
+    return False  # <--- Denied
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+SimpleLogin(app, login_checker=only_jimjams_users_login)
+
+
+
 
 
 @app.route('/')
@@ -8,3 +44,13 @@ def index():
     return """
     <p style="font-size:xx-large"> <b>Jimjams BET HQ</b> </p>
     """
+
+@app.route('/home')
+def home():
+    return """
+    <p style="font-size:xx-large"> <b>HOME</b> </p>
+    """
+
+
+
+
