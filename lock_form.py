ce8f51db4e9a7dac916ab94b228f676a646c1d64
Zach Manno <zdm5045@psu.edu>
2020-10-02 21:32:14 -0400

    add remaining validation

--- lock_form.py
+++ lock_form.py
@@ -20,10 +20,39 @@ from wtforms.validators import (DataRequired,
 #         raise ValidationError('Field must be less than 50 characters')
 
 def validate_favorite(form, field):
-    if (form.favorite.data not in [form.home.data, form.away.data]):
-        raise ValidationError('Favorite must match the home or away team')
+    if form.favorite.data:
+        if (form.favorite.data not in [form.home.data, form.away.data]):
+            raise ValidationError('Favorite must match the home or away team')
+        elif form.ou.data:
+            raise ValidationError("O/U can't have a value for a Spread pick, please switch to Over/Under and clear it out")
+        else:
+            print("favorite validated")
+
+def validate_pick(form, field):
+    if form.ou.data:
+        if form.pick.data.upper() not in ['OVER', 'UNDER']:
+            raise ValidationError("Over under pick must be OVER or UNDER")
     else:
-        print("validated")
+        if (form.pick.data not in [form.home.data, form.away.data]):
+            raise ValidationError('Pick must match the home or away team')
+        elif form.ou.data:
+            raise ValidationError("O/U can't have a value for a Spread pick, please switch to Over/Under and clear it out")
+        else:
+            print("Spread pick validated")
+
+def validate_ou(form, field):
+    if form.ou.data:
+        if form.line.data:
+            raise ValidationError("Line can't have a value for an Over/Under pick, please switch to Spread and clear it out")
+        if form.favorite.data:
+            raise ValidationError("Favorite can't have a value for an Over/Under pick, please switch to Spread and clear it out")
+        if float(form.ou.data) < 30:
+            raise ValidationError("Ehhh are you sure the O/U is that low??")
+        if float(form.ou.data) > 70:
+            raise ValidationError("Ehh are you sure the O/U is that high??")
+    print("ou validated")
+
+
 
 class LockForm(FlaskForm):
     """Lock form."""
@@ -33,14 +62,9 @@ class LockForm(FlaskForm):
     away = StringField('Away', [
         DataRequired()
         ])
-    favorite = StringField('Favorite', [
-        DataRequired(), validate_favorite])
-    pick = StringField('Pick', [
-        #DataRequired()
-        ])
-    line = FloatField('Line', [
-        #DataRequired()
-        ])
+    favorite = StringField('Favorite', [validate_favorite])
+    pick = StringField('Pick', [validate_pick])
+    line = FloatField('Line', [Optional()])
 
     ou = FloatField('O/U', [Optional()])
 
